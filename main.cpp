162e37d3926a0734439ad3904da678227ca79db2
maurelo860@gmail.com <maurelo860@gmail.com>
2026-02-14 21:23:25 -0600

    Metodos de la practica de laboratorio añadidos

--- main.cpp
+++ main.cpp
@@ -5,48 +5,68 @@ using namespace std;
 int main() 
 {
     ListaCircular lista;
-    int valor;
-    int opcion = 0;
-    int indice;
-    while (opcion != 5)
+    int valor, indice, opcion = 0;
+
+    while (opcion != 9)
     {
-        cout << "Lista Circular: " << endl;
+        cout << "Lista actual: " << endl;
         lista.mostrar();
-        cout << " MENU" << endl;
-        cout << "1. Insertar nodo" << endl;
-        cout << "2. Eliminar nodo al inicio" << endl;
-        cout << "3. Eliminar nodo al final" << endl;
-        cout << "4. Eliminar nodo en posicion especifica" << endl;
-        cout << "5. Salir" << endl;
+        cout << "   MENU    " << endl;
+        cout << "1. Insertar nodo al inicio" << endl;
+        cout << "2. Insertar nodo al medio" << endl;
+        cout << "3. Insertar nodo al final" << endl;
+        cout << "4. Eliminar nodo al inicio" << endl;
+        cout << "5. Eliminar nodo al medio" << endl;
+        cout << "6. Eliminar nodo al final" << endl;
+        cout << "7. Listar (Mostrar)" << endl;
+        cout << "8. Ver tamaño de la lista" << endl;
+        cout << "9. Salir" << endl;
         cout << "Ingrese una opción: ";
         cin >> opcion; 
+        
         switch (opcion) 
         {
             case 1:
                 cout << "Ingrese el valor a insertar: ";
                 cin >> valor;
-                lista.insertar(valor);
-                cout << endl;
+                lista.insertarInicio(valor);
                 break;
             case 2:
-                lista.eliminarInicio();
-                cout << endl;
+                cout << "Ingrese el valor: ";
+                cin >> valor;
+                cout << "Ingrese la posición: ";
+                cin >> indice;
+                lista.insertarMedio(valor, indice);
                 break;
             case 3:
-                lista.eliminarFinal();
-                cout << endl;
+                cout << "Ingrese el valor a insertar: ";
+                cin >> valor;
+                lista.insertarFinal(valor);
                 break;
             case 4:
+                lista.eliminarInicio();
+                break;
+            case 5:
                 cout << "Ingrese la posición del nodo a eliminar: ";
                 cin >> indice;
                 lista.eliminarMedio(indice);
-                cout << endl;
                 break;
-            case 5:
+            case 6:
+                lista.eliminarFinal();
+                break;
+            case 7:
+                cout << "Lista actual: ";
+                lista.mostrar();
+                break;
+            case 8:
+                cout << "Tamaño de la lista: " << lista.obtenerTamaño() << endl;
+                break;
+            case 9:
                 cout << "Saliendo del programa." << endl;
                 break;
             default:
                 cout << "Opción no válida." << endl;
         }
     }
+    return 0;
 }
